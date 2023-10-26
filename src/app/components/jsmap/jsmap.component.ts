3cb4f8f598f0692e0e054141a50a67810bf4d30b
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-10-26 10:08:55 +0700

    Update responsive map

--- src/app/components/jsmap/jsmap.component.ts
+++ src/app/components/jsmap/jsmap.component.ts
@@ -1,36 +1,37 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import H from '@here/maps-api-for-javascript';
+import onResize from 'simple-element-resize-detector';
 @Component({
   selector: 'app-jsmap',
   templateUrl: './jsmap.component.html',
-  styleUrls: ['./jsmap.component.css']
+  styleUrls: ['./jsmap.component.css'],
 })
 export class JsmapComponent implements OnInit {
   private map?: H.Map;
 
   @ViewChild('map') mapDiv?: ElementRef;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   ngAfterViewInit(): void {
     if (!this.map && this.mapDiv) {
       // Instantiate a platform, default layers and a map as usual.
       const platform = new H.service.Platform({
-        apikey: 'r5vLK2bvhfo9nyS9zPyqocCEUfqz64N6mleOP6sSNEA'
+        apikey: 'r5vLK2bvhfo9nyS9zPyqocCEUfqz64N6mleOP6sSNEA',
       });
       const layers = platform.createDefaultLayers();
       const map = new H.Map(
         this.mapDiv.nativeElement,
-        // Add type assertion to the layers object...
-        // ...to avoid any Type errors during compilation.
         (layers as any).vector.normal.map,
         {
           pixelRatio: window.devicePixelRatio,
-          center: {lat: 0, lng: 0},
+          center: { lat: 0, lng: 0 },
           zoom: 2,
-        },
+        }
       );
+      onResize(this.mapDiv.nativeElement, () => {
+        map.getViewPort().resize();
+      });
       this.map = map;
     }
   }
