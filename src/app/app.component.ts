c0240fc03da8e5767589268f52f1c90d573458fb
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-10-27 13:51:09 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
-import { GeocodingService } from './services/geocoding.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  address = 'Invalidenstr 117 Berlin';
-
-  constructor(private geocodingService: GeocodingService) {}
-
-  ngOnInit() {
-    this.geocodingService
-      .getLocation(this.address)
-      .subscribe((response: any) => {
-        console.log('---getLocation', response);
-      });
-  }
+  title = 'ads-management';
 }
--- src/app/components/jsmap/jsmap.component.ts
+++ src/app/components/jsmap/jsmap.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import H from '@here/maps-api-for-javascript';
 import onResize from 'simple-element-resize-detector';
 import { hereMapAPI } from 'src/apis/hereMapAPI';
+import { GeocodingService } from 'src/app/services/geocoding.service';
+
 @Component({
   selector: 'app-jsmap',
   templateUrl: './jsmap.component.html',
@@ -11,29 +13,46 @@ export class JsmapComponent implements OnInit {
   private map?: H.Map;
   public keyAPI = hereMapAPI;
   @ViewChild('map') mapDiv?: ElementRef;
-  constructor() {}
+  address = 'Invalidenstr 117 Berlin';
+  resultAddress: any;
+  constructor(private geocodingService: GeocodingService) {
+    this.geocodingService
+      .getLocation(this.address)
+      .subscribe((response: any) => {
+        this.resultAddress = response.items[0];
+        console.log('---resultAddress', this.resultAddress);
+      });
+  }
 
   ngOnInit() {}
   ngAfterViewInit(): void {
-    if (!this.map && this.mapDiv) {
-      // Instantiate a platform, default layers and a map as usual.
-      const platform = new H.service.Platform({
-        apikey: this.keyAPI.apiKey,
-      });
-      const layers = platform.createDefaultLayers();
-      const map = new H.Map(
-        this.mapDiv.nativeElement,
-        (layers as any).vector.normal.map,
-        {
-          pixelRatio: window.devicePixelRatio,
-          center: { lat: 0, lng: 0 },
-          zoom: 2,
+    this.geocodingService
+      .getLocation(this.address)
+      .subscribe((response: any) => {
+        this.resultAddress = response.items[0];
+        if (!this.map && this.mapDiv) {
+          // Instantiate a platform, default layers and a map as usual.
+          const center = this.resultAddress
+            ? this.resultAddress.position
+            : { lat: 0, lng: 0 };
+          const platform = new H.service.Platform({
+            apikey: this.keyAPI.apiKey,
+          });
+          const layers = platform.createDefaultLayers();
+          const map = new H.Map(
+            this.mapDiv.nativeElement,
+            (layers as any).vector.normal.map,
+            {
+              pixelRatio: window.devicePixelRatio,
+              center: center,
+              zoom: 2,
+            }
+          );
+          onResize(this.mapDiv.nativeElement, () => {
+            map.getViewPort().resize();
+          });
+          this.map = map;
         }
-      );
-      onResize(this.mapDiv.nativeElement, () => {
-        map.getViewPort().resize();
       });
-      this.map = map;
-    }
   }
 }
